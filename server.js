62e2177be00217a46f881dba585037ccb04f6216
Mike <print@3daddict.com>
2021-03-21 09:16:01 -0700

    fetching product with var

--- server.js
+++ server.js
@@ -31,4 +31,47 @@ app.get("/shop-info", (req, res) => {
       });
   });
 
+  app.get("/products", (req, res) => {
+    fetch(`https://${process.env.SHOP}/admin/api/graphql.json`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Shopify-Access-Token": `${process.env.TOKEN}`
+      },
+      body: JSON.stringify({
+        query: `query findProducts(
+          $query: 
+            String!, 
+            $num: Int!
+            ) 
+          {
+           shop {
+             name
+           }
+           products(first: $num, query: $query) {
+             edges {
+               cursor
+               node {
+                 id
+                 title
+                 featuredImage {
+                   originalSrc
+                 }
+                 totalInventory
+               }
+             }
+           }
+         }`,
+        variables: { query: "shirt", num: 5 }
+      })
+    })
+      .then(result => {
+        return result.json();
+      })
+      .then(data => {
+        console.log("data returned:\n", data);
+        res.send(data);
+      });
+  });
+
 app.listen(3000, () => console.log("Listening on port 3000 .... "));
